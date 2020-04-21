ace23055133fa7fac7acda08de2c3bca96bb1bd2
tanguy jouvin <tanguyJouvin@protonmail.com>
2020-04-21 16:34:30 +0200

    resolve exercise

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,19 +1,34 @@
 package com.wildcodeschool.doctor.controller;
 
+import java.util.List;
+
 import com.fasterxml.jackson.databind.util.ClassUtil.Ctor;
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
+    @GetMapping("/doctor/")
+   
+    public String doctor(){
+        return "index.html";
+    }
+
     @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor(@PathVariable int number) {
-
-        return Doctor.getNumber(number);
+    Doctor getDoctorByNumber(@PathVariable int number) {
+        if ( number <= 12) {
+            throw new ResponseStatusException(HttpStatus.FOUND, "Doctor" + number +"Found");
+        } else if ( number == 13){
+            return new Doctor(13, "Jodie Whittaker"); 
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number); 
     }
 }
