645538830a19d1a440a9a0f7696f795fb67e2dc6
tanguy jouvin <tanguyJouvin@protonmail.com>
2020-04-21 17:28:34 +0200

    resolve exercise

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -10,22 +10,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-   
-    public String doctor(){
-        return "index.html";
-    }
-
     @GetMapping("/doctor/{number}")
     @ResponseBody
-    Doctor getDoctorByNumber(@PathVariable int number) {
+    @ResponseStatus
+    Doctor getDoctorByNumber(@PathVariable("number") final int number) {
         if ( number <= 12) {
-            throw new ResponseStatusException(HttpStatus.FOUND, "Doctor" + number +"Found");
+            throw new ResponseStatusException(HttpStatus.FOUND, "Doctor " + number + " Found");
         } else if ( number == 13){
             return new Doctor(13, "Jodie Whittaker"); 
         }
