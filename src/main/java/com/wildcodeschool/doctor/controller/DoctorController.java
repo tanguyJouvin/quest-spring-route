a1ff5256c571e7d570781c8e7f9250de385d37c9
tanguy jouvin <tanguyJouvin@protonmail.com>
2020-04-21 15:53:03 +0200

    first commit

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,19 @@
 package com.wildcodeschool.doctor.controller;
 
+import com.fasterxml.jackson.databind.util.ClassUtil.Ctor;
 import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
 
-        return null;
+        return Doctor.getNumber(number);
     }
 }
